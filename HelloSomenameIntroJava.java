eedceda860da04aa5ea9a2a94e0f92987b4c782f
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2020-04-15 12:00:32 +1000

    Update HelloSomenameIntroJava.java

--- HelloSomenameIntroJava.java
+++ HelloSomenameIntroJava.java
@@ -2,7 +2,7 @@ public class HelloSomenameIntroJava {
   //  Public class activated with custom name //    
   // Public static void = method of one or more command(s) //             
     public static void main(String[] args) {
-    System.out.print("Hello Safin. Welcome to the basics of Java™ programming.");
+    System.out.print("Hello. Welcome to the basics of Java™ programming.");
   /* System.out.print = Printing all characters enclosed in the brackes and double-quotes.
      System.out.pritln = Printing all characters enclosed in the first line of double-quotes. */                                                                                                 
                                                                                                         
