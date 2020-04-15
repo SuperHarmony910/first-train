09eea36a53af039860c38985dda0b79b5e152f46
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2020-04-15 11:47:24 +1000

    Create Creator.java

---
+++ Creator.java
@@ -0,0 +1,11 @@
+public class Creator {
+    public static void main(String[] args) {
+    String name = "Safin Zaman";
+    int yearBorn = 2008;
+    System.out.print("Hi! My name is ");
+    System.out.print(name);
+    System.out.print(". I am a keen coder and I was born in ");
+    System.out.print(yearBorn);
+    System.out.print(".");
+    }
+}
